ff0cb14438b722b390fb2d31f7541fa9f3983dba
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-08-01 16:28:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Input extends React.Component {
+
+  render() {
+    return (
+      <div className="third">
+        <h2>{this.props.color}</h2>
+        <input type="text" onChange={this.props.onChange} />
+      </div>
+    );
+  }
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      red: 255,
+      green: 255,
+      blue: 255,
+    }
+  }
+
+  changeColor = function (event, color) {
+    switch(color) {
+      case 'red':
+        this.setState({
+          red: event.target.value
+        });
+      break;
+      case 'green':
+        this.setState({
+          green: event.target.value
+        });
+        break;
+      case 'blue':
+        this.setState({
+          blue: event.target.value
+        });
+        break;
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Title</h1>
+        </div>
+        <div>
+          <Input color='red' onChange={(e) => this.changeColor(e, 'red')}/>
+          <Input color='green' onChange={(e) => this.changeColor(e, 'green')}/>
+          <Input color='blue' onChange={(e) => this.changeColor(e, 'blue')}/>
+        </div>
+        <div className="boxcont">
+          <Box red={this.state.red} green={this.state.green} blue={this.state.blue}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Box extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      red: props.red,
+      green: props.green,
+      blue: props.blue,
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      red: newProps.red,
+      green: newProps.green,
+      blue: newProps.blue,
+  });
+}
+
+  returnColor(red, green, blue) {
+    return (
+      "rgb(" + red + "," + green + "," + blue + ")"
+    )
+  }
+
+  render() {
+    return (
+      <div className="box" style={{backgroundColor: this.returnColor(this.state.red, this.state.green, this.state.blue,)}}></div>
+    );
+  };
+};
+
+
+export default App;
